af397b5444c95a9d937c6c0cecd897fbb3240419
Pedro Meira <petergamer8k@gmail.com>
2022-10-25 21:38:49 -0300

    chore: configuring styled components & design tokens

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  body {
+    color: ${props => props.theme['base-text']};
+    background-color: ${props => props.theme['base-background']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+  }
+
+`
