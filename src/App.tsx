717b74e2970ea37657a2f58b1525553caade4c06
Pedro Meira <petergamer8k@gmail.com>
2022-10-25 21:42:54 -0300

    chore: configuring react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './routes/Router'
 import { GlobalStyles } from './styles/global'
 import { darkTheme } from './styles/themes/dark'
 
 export function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <h1>hello world</h1>
+      <Router />
       <GlobalStyles />
     </ThemeProvider>
   )
